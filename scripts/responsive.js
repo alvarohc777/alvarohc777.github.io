3d100e5969b8837bf8cc9359d87370d6487ec482
alvarohc777 <aherrada@uninorte.edu.co>
2023-02-20 01:06:54 -0500

    Update layout

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -6,23 +6,23 @@ function toogleNav() {
         sidebar.style.width = "250px";
         document.getElementById("openbtn").innerHTML = "x";
         document.getElementById("mainContent").style.marginLeft = "250px";
-        document.getElementById('footer').style.marginLeft = '250px';
+        // document.getElementById('footer').style.marginLeft = '250px';
 
         sidebarState = "open"
     } else {
         sidebar.style.width = "0";
         document.getElementById("openbtn").innerHTML = "&#9002;&#9002;&#9002;";
         document.getElementById("mainContent").style.marginLeft = "0px";
-        document.getElementById('footer').style.marginLeft = "0";
+        // document.getElementById('footer').style.marginLeft = "0";
         sidebarState = "closed"
     }
 }
 
 
 
-sidebar.style.width = "250px";
-document.getElementById("mainContent").style.marginLeft = "250px";
-document.getElementById("footer").style.marginLeft = "250px";
+// sidebar.style.width = "250px";
+// document.getElementById("mainContent").style.marginLeft = "250px";
+// document.getElementById("footer").style.marginLeft = "250px";
 
 
 
